97eb5922a259377643e5de353d475f0ae74c3de5
4scencio <alessandroascencio@protonmail.com>
2021-02-18 11:21:04 -0800

    molecule socialnetworks

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <template>
   <div class="component">
-    <SocialLink
-      name="Facebook"
-      to="http://www.facebook.com"
-      icon="facebook.svg"
-    />
-    <SocialLink
-      name="Github"
-      to="http://www.Github.com"
-      icon="github.svg"
-    />
-        <SocialLink
-      name="Twitter"
-      to="http://www.Twitter.com"
-      icon="twitter.svg"
-    />
-    
+    <SocialNetworks />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SocialLink } from '@/components/atoms'
+import { SocialNetworks } from '@/components/molecules'
 
 export default defineComponent({
   name: 'App',
-  components: { SocialLink }
+  components: { SocialNetworks }
 })
 </script>
 
 <style lang="scss">
 .component {
   width: 300px;
+  margin: 5rem;
 }
 </style>
---
+++ src/components/molecules/SocialNetworks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="social-networks">
+    <SocialLink name="Github" to="http://www.Github.com" icon="github.svg" />
+    <SocialLink name="Twitter" to="http://www.Twitter.com" icon="twitter.svg" />
+    <SocialLink
+      name="Facebook"
+      to="https://www.Facebook.com"
+      icon="facebook.svg"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { SocialLink } from '@/components/atoms'
+
+export default defineComponent({
+  components: { SocialLink }
+})
+</script>
+
+<style lang="scss" scoped>
+.social-networks {
+  display: grid;
+  grid-gap: 1rem;
+}
+</style>
